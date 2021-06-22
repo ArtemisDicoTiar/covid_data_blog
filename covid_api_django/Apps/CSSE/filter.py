f620e3253df5296e330f5c686fb747163c81a16d
DicoTiar <mw18074@bristol.ac.uk>
2021-06-22 19:21:52 +0100

    CSSE accuracy endpoint implemented (finished) #16

--- covid_api_django/Apps/CSSE/filter.py
+++ covid_api_django/Apps/CSSE/filter.py
@@ -1,14 +1,15 @@
 from datetime import datetime, timedelta
+from django.http import HttpResponseBadRequest
 
 
 def prediction_parameter_validate(predictedDate: str, date: str, offset: int):
     if offset > 7:
-        raise ValueError("Offset should be smaller than 7 days")
+        return HttpResponseBadRequest("Offset should be smaller than 7 days")
 
     if datetime.strptime(predictedDate, '%Y-%m-%d').date() > datetime.strptime(date, '%Y-%m-%d').date():
-        raise ValueError("Predicted date should be earlier than date.")
+        return HttpResponseBadRequest("Predicted date should be earlier than date.")
 
     if (datetime.strptime(date, '%Y-%m-%d') + timedelta(days=offset - 1)).date() \
             - datetime.strptime(predictedDate, '%Y-%m-%d').date() \
             > timedelta(days=7):
-        raise ValueError("Only 7 days can be viewed from predicted date")
+        return HttpResponseBadRequest("Only 7 days can be viewed from predicted date")
