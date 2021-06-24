b24d2d4565b4cc253fb0ae2c91b7a3297195b100
DicoTiar <mw18074@bristol.ac.uk>
2021-06-24 23:47:45 +0100

    jenkins publish endpoint update #16

--- covid_api_django/publishBuild.sh
+++ covid_api_django/publishBuild.sh
@@ -1,6 +1,6 @@
 docker stop covid_api_django_django_api_1
 docker rm covid_api_django_django_api_1
 
-docker rmi covid_api_django_django_api
+#docker rmi covid_api_django_django_api
 
 docker-compose up -d
