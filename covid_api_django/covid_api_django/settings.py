e3a82ef9e312f727ec407517ad8a4a8827e7366e
DicoTiar <mw18074@bristol.ac.uk>
2021-06-24 19:00:57 +0100

    Docker static files lost fixing #16

--- covid_api_django/covid_api_django/settings.py
+++ covid_api_django/covid_api_django/settings.py
@@ -146,4 +146,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/api/static/'
+STATIC_URL = '/static/'
+
+# USE_X_FORWARDED_HOST = True
